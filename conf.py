cbf94feb728e2528c4a555d3a282cb2088340d27
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-18 10:45:47 -0400

    add triage issue template

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Version 2.0, May 2020
 # -*- coding: utf-8 -*-
 #
 # General information about the project.
@@ -9,13 +10,13 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'Introduction to Git as topic_tutorial'
-copyright = '2019, CyVerse'
-author = 'Meghan Balk, Ramona Walls'
-version = '1.0'
-release = '1.0'
+project = 'YOUR GUIDE NAME HERE'
+copyright = '2020, CyVerse'
+author = 'your_name'
+version = '2.0'
+release = '2.0'
 
-epub_title = 'Git for Mere Mortals'
-epub_author = 'Meghan Balk, Ramona Walls'
-epub_publisher = 'CyVerse'
-epub_copyright = '2019'
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
